7e5a564ae286c3384e36c614d81e26e1c3ddf507
Blake Elias <blakeelias@gmail.com>
2011-09-24 15:33:36 -0400

    Replaced some parts of the orbit function with Vfunc calls.  Can't test yet.

--- main.c
+++ main.c
@@ -105,16 +105,10 @@ angleTarget = currentAngle + angleStep;
 
 posTarget[0] = radius * cos(angleTarget) + center[0];
 posTarget[1] = radius * sin(angleTarget) + center[1];
-
-velTarget[0] = posTarget[0] - myState[0];
-velTarget[1] = posTarget[1] - myState[1];
-velTarget[2] = posTarget[2] - myState[2];
-mathVecNormalize(velTarget, 3);
-velTarget[0] *= radius * desiredAngularVelocity;
-velTarget[1] *= radius * desiredAngularVelocity;
-velTarget[2] *= radius * desiredAngularVelocity;
-
-ZRSetVelocityTarget(velTarget);
+ 
+ VPoint(myState, posTarget, velTarget);
+ VMult(velTarget, radius * desiredAngularVelocity, velTarget);
+ ZRSetVelocityTarget(velTarget);
 //END::PROC::Orbit
 }
 
